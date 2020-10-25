92ce69fcfa9e596c2eb2053714d7b3d02b7ea088
LarsFlieger <lars.flieger7@gmail.com>
2020-10-25 13:45:40 +0100

    TextureAtlas + TextureRegion + Camera (Basics)

--- core/src/com/michael/javafrank/Tutorial.java
+++ core/src/com/michael/javafrank/Tutorial.java
@@ -3,15 +3,26 @@ package com.michael.javafrank;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 
 public class Tutorial extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture texture;
+	private TextureAtlas atlas;
+	private TextureRegion pagerImage;
+	private TextureRegion telfonImage;
+
 	private Sprite sprite;
+	private Sprite secondSprite;
+
+	private BitmapFont font;
+
+	private OrthographicCamera camera;
+
+	private final int VIEWPORT_WIDTH = 10;
 
 	@Override
 	public void create () {
@@ -21,17 +32,37 @@ public class Tutorial extends ApplicationAdapter {
 		Gdx.app.log("Density", "" + Gdx.graphics.getDensity());
 
 		batch = new SpriteBatch();
+
+		atlas = new TextureAtlas(Gdx.files.internal("atlas.atlas"));
+		for(Texture texture : atlas.getTextures()) {
+			texture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+		}
+
+		pagerImage = atlas.findRegion("pager");
+		telfonImage = atlas.findRegion("telefon");
+
 		Pixmap pixmap = createPixmap();
 		texture = new Texture(pixmap);
 		pixmap.dispose();
 		texture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
 
-		sprite = new Sprite(texture);
-		sprite.setPosition(200, 200);
-		sprite.setSize(128 * 4, 128 * 4);
-		sprite.setOrigin(128 * 2, 128 * 2);
+		sprite = new Sprite(pagerImage);
+		sprite.setPosition(1, 1);
+		sprite.setSize(2, 2);
+		sprite.setOrigin(1, 1);
 		sprite.setRotation(0);
+
+
+		secondSprite = new Sprite(telfonImage);
+		secondSprite.setPosition(1, 1);
+		secondSprite.setSize(2, 2);
+
+		font = new BitmapFont(Gdx.files.internal("fonts/font.fnt"));
+
+		camera = new OrthographicCamera(VIEWPORT_WIDTH, VIEWPORT_WIDTH * Gdx.graphics.getHeight()/(float)Gdx.graphics.getWidth());
+		camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0);
+		camera.update();
 	}
 
 	private Pixmap createPixmap() {
@@ -49,6 +80,7 @@ public class Tutorial extends ApplicationAdapter {
 
 		return pixmap;
 	}
+	float i = 0;
 
 	@Override
 	public void render () {
@@ -58,14 +90,25 @@ public class Tutorial extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		batch.setProjectionMatrix(camera.combined);
+
+		camera.position.set(i -= 0.01f, camera.viewportHeight/2f, 0);
+		camera.update();
+
 		batch.begin();
 		sprite.draw(batch);
+		secondSprite.draw(batch);
+		font.setColor(1,1,1,1);
+		font.getData().setScale(0.02f);
+		GlyphLayout layout = new GlyphLayout(font, "Hello!");
+		font.draw(batch, layout, 0, layout.height);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		atlas.dispose();
 	}
 
 	@Override
@@ -79,5 +122,7 @@ public class Tutorial extends ApplicationAdapter {
 
 	@Override
 	public void resize(int width, int height) {
+		camera.viewportHeight = VIEWPORT_WIDTH * height/(float)width;
+		camera.update();
 	}
 }
--- desktop/src/com/michael/javafrank/desktop/DesktopLauncher.java
+++ desktop/src/com/michael/javafrank/desktop/DesktopLauncher.java
@@ -3,31 +3,47 @@ package com.michael.javafrank.desktop;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.michael.javafrank.Tutorial;
 
 public class DesktopLauncher {
+
+	public static final boolean CREATE_ATLAS = false;
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.title = "Example";
-		config.width = 1920;
-		config.height = 1080;
-		config.resizable = true;
-		config.fullscreen = false;
-		config.samples = 2; //anti alasing
-		config.vSyncEnabled = true;
-		config.foregroundFPS = 60;
-		config.backgroundFPS = 30;
-		config.pauseWhenBackground = true; //Require this to pause and resume on focus
+		if(CREATE_ATLAS) {
+			TexturePacker.Settings settings = new TexturePacker.Settings();
+			settings.edgePadding = true;
+			settings.paddingX = 2;
+			settings.paddingY = 2;
+			settings.maxHeight = 2048;
+			settings.maxWidth = 2048;
+			TexturePacker.process(settings, "core\\assets\\textures","core\\assets","atlas");
 
-		config.addIcon("icons/icon_32.png", Files.FileType.Internal);
-		config.addIcon("icons/icon_16.png", Files.FileType.Internal);
-		config.addIcon("icons/icon_128.png", Files.FileType.Internal);
+		} else {
+			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		//position where the windows spawns
-		config.x = 0;
-		config.y = 0;
+			config.title = "Example";
+			config.width = 1920;
+			config.height = 1080;
+			config.resizable = true;
+			config.fullscreen = false;
+			config.samples = 2; //anti alasing
+			config.vSyncEnabled = true;
+			config.foregroundFPS = 60;
+			config.backgroundFPS = 30;
+			config.pauseWhenBackground = true; //Require this to pause and resume on focus
 
-		new LwjglApplication(new Tutorial(), config);
+			config.addIcon("icons/icon_32.png", Files.FileType.Internal);
+			config.addIcon("icons/icon_16.png", Files.FileType.Internal);
+			config.addIcon("icons/icon_128.png", Files.FileType.Internal);
+
+			//position where the windows spawns
+			config.x = 0;
+			config.y = 0;
+
+			new LwjglApplication(new Tutorial(), config);
+		}
 	}
 }
